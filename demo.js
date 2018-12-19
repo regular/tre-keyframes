1e98881612d5fe444ce386e19097f52e10bae1ee
Jan Bölsche <regular.gonzales@googlemail.com>
2018-12-19 17:03:41 +0100

    Add json editor

--- demo.js
+++ demo.js
@@ -2,14 +2,40 @@ const {client} = require('tre-client')
 const Finder = require('tre-finder')
 const {RenderTimeline, renderPropertyTree} = require('tre-timeline')
 const h = require('mutant/html-element')
+const Value = require('mutant/value')
+const computed = require('mutant/computed')
+const MutantArray = require('mutant/array')
 const setStyle = require('module-styles')('tre-keyframes-demo')
 const Keyframes = require('.')
 const {makePane, makeDivider, makeSplitPane} = require('tre-split-pane')
+const Editor = require('tre-json-editor')
+const Shell = require('tre-editor-shell')
+require('brace/theme/solarized_dark')
 
 client( (err, ssb, config) => {
   if (err) return console.error(err)
 
   const renderKeyframes = Keyframes(ssb)
+
+  const renderEditor = Editor(null, {
+    ace: {
+      theme: 'ace/theme/solarized_dark',
+      tabSize: 2,
+      useSoftTabs: true
+    }
+  })
+
+  let current_kv
+  const selectedFrameObs = Value()
+  const renderShell = Shell(ssb, {
+    save: (kv, cb) => {
+      ssb.publish(kv.value.content, (err, kv) => {
+        if (err) return cb(err)
+        selectedFrameObs.set(kv)
+        cb(null, kv)
+      })
+    }
+  })
 
   const renderFinder = Finder(ssb, {
     details: kv => {
@@ -27,23 +53,32 @@ client( (err, ssb, config) => {
 
   const renderTimeline = RenderTimeline(ssb)
 
+  const framesObs = MutantArray()
   const items = renderKeyframes({
     key: config.tre.branches.animation
+  }, {
+    selectedFrameObs,
+    framesObs
   })
 
   document.body.appendChild(h('.tre-keyframes-editor', {
     hooks: [el => el => {items.abort()}],
     'ev-timeline-click': e => {
       console.warn(e)
-      if (e.detail.track == 0) {
+      const kv = framesObs().find( kv => kv.value.content.frame == e.detail.frame )
+      if (kv) {
+        console.warn('selected', kv)
+        selectedFrameObs.set(kv)
+      } else {
         ssb.publish({
           type: 'keyframe',
           'part-of': config.tre.branches.animation,
           root: config.tre.branches.root,
           frame: e.detail.frame
-        }, (err, msg) => {
+        }, (err, kv) => {
           if (err) return console.error(err)
-          console.log('Published', msg)
+          console.log('Published', kv)
+          selectedFrameObs.set(kv)
         })
       }
     }
@@ -67,7 +102,16 @@ client( (err, ssb, config) => {
             width: '100%',
             margin: 0
           }
-        }, 'Editor')
+        }, [
+          computed(selectedFrameObs, kv => {
+            if (!kv) return []
+            if (revisionRoot(kv) == revisionRoot(current_kv)) return computed.NO_CHANGE
+            current_kv = kv
+            console.warn('rendering editor shell for', kv)
+            const contentObs = Value(Object.assign({}, kv.value.content))
+            return renderShell(kv, {renderEditor, contentObs})
+          })
+        ])
       ])
     ])
   ]))
@@ -88,7 +132,7 @@ setStyle(`
     height: 0px;
   }
   .tre-keyframes-editor {
-    height: 12em;
+    height: 18em;
     width: 100%;
   }
   .tre-finder-with-timeline {
@@ -102,5 +146,30 @@ setStyle(`
     width: 100%;
     overflow-y: auto;
   }
+
+  .tre-editor-shell {
+    width: 100%;
+    height: 100%;
+  }
+  .tre-editor-shell .operations li span {
+    margin-right: .5em;
+  }
+  .tre-editor-shell .new-revision {
+    background: #B9A249;
+    padding: 1em;
+    margin-bottom: 1em;
+  }
+  .operations span.path {
+    font-family: monospace;
+  }
+  .operations span.value.string:before {
+    content: "\\"";
+  }
+  .operations span.value.string:after {
+    content: "\\"";
+  }
 `)
 
+function revisionRoot(kv) {
+  return kv && kv.value.content && kv.value.content.revisionRoot || kv && kv.key
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const pull = require('pull-stream')
 const MutantArray = require('mutant/array')
 const MutantMap = require('mutant/map')
 const h = require('mutant/html-element')
+const Value = require('mutant/value')
 const computed = require('mutant/computed')
 const collectMutations = require('collect-mutations')
 const setStyles = require('module-styles')('tre-keyframes')
@@ -13,18 +14,26 @@ setStyles(`
   .tre-timeline .frameSlot.has-keyframe:hover {
     background: rgba(250,250,200,0.6);
   }
+  .tre-timeline .frameSlot.selected {
+    background: rgba(255, 255, 50, 0.8);
+    box-shadow: 0 0 18px gold;
+  }
+  .tre-timeline .frameSlot.selected:hover {
+    background: rgba(255, 255, 100, 0.9);
+    box-shadow: 0 0 24px gold;
+  }
 `)
 
 module.exports = function(ssb, opts) {
   opts = opts || {}
   return function(kv, ctx) {
-    console.log('keyframes for', kv)
     ctx = ctx || {}
-    const keyframes = MutantArray()
+    const keyframes = ctx.framesObs || MutantArray()
+    const selectedFrameObs = ctx.selectedFrameObs || Value()
 
     const drain = collectMutations(keyframes, {sync: true})
     pull(
-      ssb['tre-parts'].byAggregationAndType(kv.key, 'keyframe', {
+      ssb['tre-parts'].byAggregationAndType(revisionRoot(kv), 'keyframe', {
         live: true,
         sync: true
       }),
@@ -33,25 +42,32 @@ module.exports = function(ssb, opts) {
     )
 
     const els = MutantMap(keyframes, kvObs => {
-      const kv = kvObs()
-      const frame = kv.value.content.frame
-      if (!Number.isFinite(frame)) return []
-      return h('.keyframe', {
-        style: {
-          'grid-row': '1 / span 1',
-          'grid-column': `${frame + 2} / span 1`
-        }
+      const frame = computed(kvObs, kv => kv && kv.value.content.frame )
+      const gridCol = computed(frame, frame => `${frame + 2} / span 1`)
+      return computed(frame, frame => {
+        if (!Number.isFinite(frame)) return []
+        return h('.keyframe', {
+          style: {
+            'grid-row': '1 / span 1',
+            'grid-column': gridCol
+          }
+        })
       })
     })
     els.abort = function() {
       drain.abort()
     }
     els.columnClasses = function(column) {
-      return computed(keyframes, kfs => {
+      return computed([keyframes, selectedFrameObs], (kfs, kv) => {
+        if (kv && kv.value.content.frame == column ) return ['selected']
         const found = kfs.find( kv => kv && kv.value.content.frame == column )
         return found ? ['has-keyframe'] : []
       })
     }
     return els
   }
+}
+
+function revisionRoot(kv) {
+  return kv && kv.value && kv.value.content && kv.value.content.revisionRoot || kv && kv.key
 }
