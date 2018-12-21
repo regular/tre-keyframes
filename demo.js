63b4a7d91fe4996a1bf3d15d11db9acab161aa23
Jan Bölsche <regular.gonzales@googlemail.com>
2018-12-21 10:27:33 +0100

    Add mini-map, improve syling and scroll behaviour

--- demo.js
+++ demo.js
@@ -5,6 +5,7 @@ const h = require('mutant/html-element')
 const Value = require('mutant/value')
 const computed = require('mutant/computed')
 const MutantArray = require('mutant/array')
+const MutantMap = require('mutant/map')
 const setStyle = require('module-styles')('tre-keyframes-demo')
 const Keyframes = require('.')
 const {makePane, makeDivider, makeSplitPane} = require('tre-split-pane')
@@ -13,6 +14,38 @@ const Shell = require('tre-editor-shell')
 const pointer = require('json8-pointer')
 const {createValue,interpolate,addFramePaths} = require('./lib/interpolate')
 require('brace/theme/solarized_dark')
+
+function renderMiniMap(framesObs, columnCountObs) {
+  const range = computed(framesObs, kvs => {
+    const frames = kvs.map( kv => kv.value.content.frame )
+    return {
+      min: Math.min(...frames), 
+      max: Math.max(...frames)
+    }
+  })
+
+  const width = computed([range, columnCountObs], ({min, max}, cols) => {
+    const percent = 100 * cols / (max - min)
+    return `${percent}%`
+  })
+
+  return h('.tre-timeline-minimap', {}, [
+    h('.visible', {
+      style: { width }
+    }),
+    MutantMap(framesObs, kvObs => {
+      console.warn('MAP')
+      const left = computed([kvObs, range], (kv, {min, max}) => {
+        const count = max - min
+        const percent = (kv.value.content.frame - min) * 100 / count
+        return `${percent}%`
+      })
+      return h('.minimap-keyframe', {
+        style: {left}
+      })
+    })
+  ])
+}
 
 function renderToolbar(scaleObs) {
   const scaleSlider = h('.tre-timeline-scale', [
@@ -80,6 +113,7 @@ client( (err, ssb, config) => {
   const contentObs = Value()
   const tracksObs = MutantArray()
   const cellWidthObs = Value(1)
+  const columnCountObs = Value(1)
   const hoverFrame = Value()
 
   const keyframeInfex = computed(framesObs, keyframes => {
@@ -123,13 +157,13 @@ client( (err, ssb, config) => {
     hooks: [el => el => {items.abort()}],
     'ev-mousemove': e => {
       const slot = e.currentTarget.querySelector('.frameSlot:hover')
-      if (!slot) return hoverFrame.set(null)
+      if (!slot) return //hoverFrame.set(null)
       const col = slot.style['grid-column-start']
       hoverFrame.set(col-2)
     },
     'ev-timeline-click': e => {
       console.warn(e)
-      const kv = framesObs().find( kv => kv.value.content.frame == e.detail.frame )
+      const kv = framesObs().find( kv => kv.value.content.frame == e.detail.column )
       if (kv) {
         if (kv !== selectedFrameObs()) {
           console.warn('selected', kv)
@@ -152,7 +186,7 @@ client( (err, ssb, config) => {
           type: 'keyframe',
           'part-of': config.tre.branches.animation,
           root: config.tre.branches.root,
-          frame: e.detail.frame
+          frame: e.detail.column
         }, (err, kv) => {
           if (err) return console.error(err)
           console.log('Published', kv)
@@ -163,17 +197,23 @@ client( (err, ssb, config) => {
   }, [
     makeSplitPane({horiz: true}, [
       makePane('75%', [
-        renderToolbar(cellWidthObs),
-        h('div.tre-finder-with-timeline', [
-          finder,
-          renderTimeline(null, {
-            tree_element: finder,
-            tracksObs,
-            cellWidthObs,
-            items,
-            columnClasses: items.columnClasses,
-            renderTrackControls
-          })
+        h('.tre-keyframes-bar', [
+          renderToolbar(cellWidthObs),
+          renderMiniMap(framesObs, columnCountObs)
+        ]),
+        h('.tre-keyframes-vscroll', [
+          h('.tre-finder-with-timeline', [
+            finder,
+            renderTimeline(null, {
+              tree_element: finder,
+              tracksObs,
+              cellWidthObs,
+              columnCountObs,
+              items,
+              columnClasses: items.columnClasses,
+              renderTrackControls
+            })
+          ])
         ])
       ]),
       makeDivider(),
@@ -209,13 +249,50 @@ setStyle(`
     font-family: sans-serif;
     font-size: 12pt;
   }
+
   .tre-keyframes-editor ::-webkit-scrollbar {
     width: 0px;
     height: 0px;
   }
-  .tre-keyframes-editor {
-    height: 18em;
+
+  .tre-keyframes-vscroll {
+    padding: 0;
+    margin: 0;
+    max-height: 16em;
+    background: green;
     width: 100%;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }
+  .tre-keyframes-editor {
+    width: 100%;
+  }
+
+  .tre-timeline-toolbar {
+    grid-column: 1 / 2;
+  }
+
+  .tre-timeline-minimap {
+    grid-column: 2 / 3;
+    position: relative;
+    background: blue;
+    overflow-x: hidden;
+  }
+
+  .tre-timeline-minimap > .minimap-keyframe {
+    position: absolute;
+    width: 1px;
+    height: 100%;
+    background: white;
+  }
+
+  .tre-timeline-minimap > .visible {
+    box-sizing: border-box;
+    z-index: 1;
+    position: absolute;
+    height: 100%;
+    background: rgba(255,255,255, 0.3);
+    border: 1px solid black;
   }
 
   .track-controls {
@@ -251,16 +328,27 @@ setStyle(`
     color: #ddd;
   }
   
+  .tre-keyframes-bar {
+    display: grid;
+    grid-template-columns: 12em 1fr;
+    grid-template-rows: 1.4em;
+    place-content: stretch;
+    place-items: stretch;
+    background: #333;
+    color: #666;
+    width: 100%;
+  }
+  
   .tre-finder-with-timeline {
     display: grid;
-    grid-template-columns: 10em auto;
-    grid-template-rows: 100%;
+    grid-template-columns: 12em 1fr;
+    grid-template-rows: 1fr;
     grid-auto-flow: column;
     place-content: stretch;
     place-items: stretch;
-    background: gold;
+    color: #111;
+    background: #777;
     width: 100%;
-    overflow-y: auto;
   }
 
   .tre-editor-shell {
